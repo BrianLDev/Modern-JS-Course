0acc88c4ccbcc027f9f842f660964fa13aff80a2
Brian Leip <brianleip@gmail.com>
2021-06-22 13:10:51 -0700

    Added notes to DOM Selectors Multiple elements

--- 03-DOM/3-04 DOM Selectors Multiple Elements/app.js
+++ 03-DOM/3-04 DOM Selectors Multiple Elements/app.js
@@ -1,4 +1,4 @@
-// document.getElementsByClassName
+// document.getElementsByClassName - note the plural Elements
 
 // const items = document.getElementsByClassName('collection-item');
 // console.log(items);
@@ -6,9 +6,13 @@
 // items[0].style.color = 'red';
 // items[3].textContent = 'Hello';
 
+// NOTE - TO LIMIT THE SCOPE OF GETELEMENTS, YOU CAN USE A QUERY SELECTOR TO FIRST GET THE PARENT OBJECT WHICH WILL LIMIT GETELEMENTS TO THE SUB-OBJECTS
+
 // const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
 // console.log(listItems);
+
+// NOTE - YOU CAN MAKE THE EXACT SAME CHANGES LOOKING SOMETHING UP BY TAG NAME AS BY CLASS NAME.  TWO WAYS TO DO THE SAME THING.
 
 // document.getElementsByTagName
 // let lis = document.getElementsByTagName('li');
@@ -17,7 +21,7 @@
 // lis[0].style.color = 'red';
 // lis[3].textContent = 'Hello';
 
-// // Conver HTML Collection into array
+// // CONVERT HTML COLLECTION INTO ARRAY - ALLOWS YOU TO USE ARRAY METHODS LIKE .REVERSE AND FOREACH
 // lis = Array.from(lis);
 
 // lis.reverse();
@@ -29,12 +33,17 @@
 
 // console.log(lis);
 
+// QUERY SELECTOR ALL - ALLOWS SELECTING MULTIPLE OBJECTS USING QUERY SELECTOR (STANDARD QUERY SELECTOR ONLY RETURNS FIRST OBJECT)
 // document.querySelectorAll
 const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// NOTE - DON'T HAVE TO CONVERT QUERY SELECTORS TO AN ARRAY SINCE IT RETURNS A NODE LIST WHICH ALLOWS FOR ARRAY FUNCTIONS LIKE .FOREACH AND .REVERSE.
 
 items.forEach(function(item, index){
     item.textContent = `${index}: Hello`;
 });
+
+// EXAMPLE: CHANGE BACKGROUND COLOR OF LIST SO THAT ODD AND EVEN NUMBERS HAVE DIFFERENT SHADES OF GRAY.  CAN USE EITHER .FOREACH OR A STANDARD FOR LOOP.
 
 const liOdd = document.querySelectorAll('li:nth-child(odd)');
 const liEven = document.querySelectorAll('li:nth-child(even)');
