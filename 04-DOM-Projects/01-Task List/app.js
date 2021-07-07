01a1bf125cf106b1ec9aac7c2b4c0dc28dafe36f
Brian Leip <brianleip@gmail.com>
2021-07-06 17:59:51 -0700

    Added clear task functionality

--- 04-DOM-Projects/01-Task List/app.js
+++ 04-DOM-Projects/01-Task List/app.js
@@ -8,12 +8,14 @@ const taskInput = document.querySelector('#task');
 // Load all event listeners
 loadEventListeners();
 
-// Functino to load all event listeners
+// Function to load all event listeners
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
   // Remove task event
   taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
 }
 
 // Function to Add Task
@@ -50,9 +52,19 @@ function addTask(e) {
 function removeTask(e) {
   // make sure the click is on the X
   if (e.target.parentElement.classList.contains('delete-item')) {
-    if (confirm('Are You Sure?')) {
+    if (confirm('Are you sure?')) {
       // remove the full li (parent of parent of target)
       e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Function to Clear Tasks
+function clearTasks(e) {
+  if (confirm('Are you sure? This will delete **ALL** tasks.')) {
+    // remove all child nodes
+    while(taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
     }
   }
 }
